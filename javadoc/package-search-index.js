001979e755c26769922b73f1721f8a24a1eacaa9
alepops <alexandramariapopa8@gmail.com>
2025-05-20 02:44:41 +0300

    commit2

---
+++ javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"BusinessLogic"},{"l":"DataAcess"},{"l":"DataModel"},{"l":"org.example"},{"l":"Presentation"},{"l":"Util"}];updateSearchResults();
